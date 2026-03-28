9428fb016977183e551949449a0b35d495cf8f72
shahadf11 <f.shahadf11@gmail.com>
2026-03-28 23:49:39 +0300

    lab 4

--- apps/bookmodule/urls.py
+++ apps/bookmodule/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'books'
+
 urlpatterns = [
-    path('', views.index),
-    path('index2/<str:val1>/', views.index2),
-    path('viewbook/<int:bookId>/', views.viewbook, name='viewbook'),
+    path('', views.index, name='index'),
+    path('list_books/', views.list_books, name='list_books'),
+    path('<int:bookId>/', views.viewbook, name='view_one_book'),
+    path('aboutus/', views.aboutus, name='aboutus'),
 ]
